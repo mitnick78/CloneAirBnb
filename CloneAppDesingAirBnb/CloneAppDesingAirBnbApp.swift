7c65449ff8274bb8ec0d90fc7154c23940bdf4aa
Christophe <c.milliere>
2023-08-19 19:02:43 +0200

    Initial Commit

---
+++ CloneAppDesingAirBnb/CloneAppDesingAirBnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneAppDesingAirBnbApp.swift
+//  CloneAppDesingAirBnb
+//
+//  Created by Christophe on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CloneAppDesingAirBnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
